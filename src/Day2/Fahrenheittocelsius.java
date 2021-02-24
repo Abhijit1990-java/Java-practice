5fe8a439cfdb09a4ef87026bbe3aa9b98473579a
Abhijit1990-java <79239521+Abhijit1990-java@users.noreply.github.com>
2021-02-23 20:54:28 -0600

    Pez.java

--- src/Day2/Fahrenheittocelsius.java
+++ src/Day2/Fahrenheittocelsius.java
@@ -6,9 +6,11 @@ public class Fahrenheittocelsius {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter the value of fahrenheit");
-        double f = input.nextDouble();
-        System.out.println("Enter the value of celsius");
-        double c = input.nextDouble();
+        double fahrenheit = input.nextDouble();
+        double celsius = ((5*(fahrenheit-32.0))/9);
+        System.out.println(fahrenheit + "degree fahrenheit is equal to " + celsius + "in celsius");
+
 
     }
 }
+
